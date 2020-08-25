1d0e736601553d0de7f5ee1aedbd3e3b1d3581e4
Talmage <talmagelynn@gmail.com>
2020-08-25 08:33:21 -0600

    first commit
    pp

---
+++ AnimeStarts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimeStarts
+//
+//  Created by Talmage Hassell on 8/3/20.
+//  Copyright © 2020 Talmage Hassell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
