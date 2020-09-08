7d59409681a652973ea5532274ba01a82d6eca83
Talmage <talmagelynn@gmail.com>
2020-09-08 08:19:19 -0600

    oops
    welp

---
+++ AnimeStarts/ApiOnMenu.swift
@@ -0,0 +1,23 @@
+//
+//  ApiOnMenu.swift
+//  AnimeStarts
+//
+//  Created by Talmage Hassell on 9/1/20.
+//  Copyright © 2020 Talmage Hassell. All rights reserved.
+// THIS IS WHAT WILL CALL FOR THE API ON MENU
+
+struct Anime: Decodable {
+    let id: Int?
+    let poster: String
+    let title: String
+    let releaseDate: String
+    let overview: String
+}
+
+
+
+
+
+
+
+
